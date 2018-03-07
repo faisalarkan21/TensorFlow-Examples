ff35d7ce19754615d3de06d95ca878b2b88460cb
faisal arkan <faisalarkan21@gmail.com>
2018-03-08 06:06:31 +0700

    add tensor dashboard example

---
+++ examples/testing/tensor_dashboard_example.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+C_1 = tf.constant(5.0)
+C_2 = tf.constant(1.0)
+C_3 = tf.constant(2.0)
+
+golden_ratio = (tf.sqrt(C_1) + C_2)/C_3
+
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter('logs', sess.graph)
+    print sess.run(golden_ratio)
+    writer.close()
+
+
+# then run,
+# tensorboard --logdir=logs
+# logs is folder, with events file in it.
